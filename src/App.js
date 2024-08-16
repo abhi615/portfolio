76288bc0d4f0ae21828b85639c582fab47ec6418
B.Abhijith <b.abhijith@publicissapient.com>
2024-08-16 14:02:26 +0530

    Added portfolio  Design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import NavBar from './Components/NavBar';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Skills } from './Components/Skills';
+import { Projects } from './Components/Project';
+import { Contact } from './Components/Contact';
+import { Footer } from './Components/Footer';
+import { Banner } from './Components/Banner';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+      <div className="App">
+        <NavBar />
+        <Banner />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    
   );
 }
 
---
+++ src/Components/Project.js
@@ -0,0 +1,120 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+export const Projects = () => {
+  const projects = [
+    {
+      title: "Sunbelt Rentals",
+      description: "This is the hardware products Rental application",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Amazon clone",
+      description: "This is e-commerece apliciation",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Work Experience & Featured Projects</h2>
+
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first" >
+                        <div>
+                          <h2>Download My CV</h2>
+                          <a href="/B.Abhijith (4).pdf" download="B.Abhijith (4).pdf">
+                            <button className="download_btn">Download Resume</button>
+                          </a>
+                        </div>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second" className="tab-panel">
+                        <h3>Work Experience (FRONTEND DEVELOPER L2) </h3>
+                          <p>
+                            Collaborated on E-commerce app using React JS, HTML,
+                            SCSS, GraphQL for seamless data interaction.
+                          </p>
+                         
+                          <p>
+                            Utilized React hooks, Context API, and Redux for
+                            efficient state management and reusable code
+                            components.
+                          </p>
+                         
+                          <p>
+                            Spearheaded the meticulous practice of unit testing,
+                            leveraging Jest React library to ensure code
+                            robustness and reliability; <br /> achieved a 50%
+                            decrease in post-release defects and boosted
+                            developer productivity by 75%.
+                          </p>
+                         
+                          <p>
+                            Executed ATDD approach, reducing post-release
+                            defects by 50% through React Testing Library and
+                            Jest.
+                          </p>
+                       
+                       
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <Row>
+                          {projects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  );
+};
