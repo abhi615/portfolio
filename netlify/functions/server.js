137f2f7618642dbcc712702878b9cd3ae89e37e7
B.Abhijith <b.abhijith@publicissapient.com>
2024-08-18 12:09:10 +0530

    Added changes

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
